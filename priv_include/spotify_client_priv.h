42bf128e9a65377dfd75cc45ba35264a3dccf11e
Francisco <fran_work124@hotmail.com>
2025-05-19 12:22:08 -0300

    .

--- priv_include/spotify_client_priv.h
+++ priv_include/spotify_client_priv.h
@@ -22,26 +22,27 @@ extern "C" {
 #include "freertos/task.h"
 
 /* Exported macro ------------------------------------------------------------*/
-#define MAX_HTTP_BUFFER 8192
 // eventgroup macros
 #define ENABLE_PLAYER       (1 << 0)
 #define DISABLE_PLAYER      (1 << 1)
-#define READY_FOR_DATA      (1 << 2)
+#define WS_READY_FOR_DATA   (1 << 2)
 #define WS_CONNECT_EVENT    (1 << 3)
 #define WS_DISCONNECT_EVENT (1 << 4)
 #define WS_DATA_EVENT       (1 << 5)
-#define DATA_PROCESSED      (1 << 6)
+#define WS_DATA_CONSUMED    (1 << 6)
 #define DO_PLAY             (1 << 7)
 #define DO_PAUSE            (1 << 8)
 #define DO_NEXT             (1 << 9)
 #define DO_PREVIOUS         (1 << 10)
+#define DO_PAUSE_UNPAUSE    (1 << 11)
 
 /* Exported types ------------------------------------------------------------*/
-typedef struct
-{
-    char   value[400];
-    time_t expiresIn;
-} AccessToken;
+typedef struct {
+    uint8_t *buffer;
+    size_t buffer_size;
+    size_t current_size;
+    void * ctx;
+} evt_user_data_t;
 
 /* Exported variables declarations -------------------------------------------*/
 
